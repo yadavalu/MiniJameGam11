3d2b97cbc342af82003bab72bef4d758207e576a
Aadith Yadav <aadith.ygalu@gmail.com>
2022-07-01 18:30:41 +0200

    Added attack in entity

--- entity.py
+++ entity.py
@@ -7,6 +7,8 @@ class Entity:
 		self.surface = surface
 		self.rect = pygame.Rect(x, y, 32, 32)
 
+		self.flip = False
+
 	def move(self):
 		dx, dy = 0, 0
 		vel = 32
@@ -18,12 +20,19 @@ class Entity:
 			dy = vel
 		if keys[pygame.K_RIGHT]:
 			dx = vel
+			self.flip = False
 		if keys[pygame.K_LEFT]:
 			dx = -vel
+			self.flip = True
+		if keys[pygame.K_SPACE]:
+			self.attack()
 
 		self.rect.x += dx
 		self.rect.y += dy
 
+	def attack(self):
+		pygame.draw.rect(self.surface, (0, 0, 255), pygame.Rect(self.rect.x - (self.rect.width if self.flip else 0), self.rect.y, self.rect.width * 2, self.rect.height))
+		
 	def draw(self):
 		pygame.draw.rect(self.surface, (255, 0, 0), self.rect)
 

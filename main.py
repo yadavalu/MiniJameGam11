e7e166f14b53a0e7b3f8cd8763c87b4246b3e630
Aadith Yadav <aadith.ygalu@gmail.com>
2022-07-03 23:15:38 +0200

    Game over#

--- main.py
+++ main.py
@@ -5,8 +5,6 @@ from entity import Entity, Object
 from tilemap import TileMap
 from map import map_gen
 
-import threading
-
 pygame.init()
 pygame.mixer.init()
 
@@ -24,16 +22,20 @@ tilemap = TileMap(display, map_gen(), "gfx/tilemap.png")
 tilemap.load_tiles()
 entity = Entity(display, 384, 384, pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(0, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects)
 enemy1 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
-#enemy2 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
-#enemy3 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
-#enemy4 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
-#enemy5 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
+enemy2 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
+enemy3 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
+enemy4 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
+enemy5 = Entity(display, randint(0, display.get_width() - 32), randint(0, display.get_height() - 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(64, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects, is_enemy=True)
 sword1 = Object(display, "s", 32 * randint(0, 19), 32 * randint(0, 19), image=pygame.image.load("gfx/swords.png").subsurface(pygame.Rect(0, 0, 32, 32)))
 sword2 = Object(display, "s", 32 * randint(0, 19), 32 * randint(0, 19), image=pygame.image.load("gfx/swords.png").subsurface(pygame.Rect(32, 0, 32, 32)))
 sword3 = Object(display, "s", 32 * randint(0, 19), 32 * randint(0, 19), image=pygame.image.load("gfx/swords.png").subsurface(pygame.Rect(64, 0, 32, 32)))
 box1 = Object(display, "1", randint(0, 19) * 32, randint(0, 19) * 32, image=pygame.image.load("gfx/tilemap.png").subsurface(96, 0, 32, 32), collection_offset=(20, 0))
 box2 = Object(display, "2", randint(0, 19) * 32, randint(0, 19) * 32, image=pygame.image.load("gfx/tilemap.png").subsurface(96, 0, 32, 32), collection_offset=(0, 20))
 reciever = Entity(display, 32 * round(randint(0, display.get_width() - 32) / 32), 32 * round(randint(0, display.get_height() - 32) / 32), pygame.image.load("gfx/delivery.png").subsurface(pygame.Rect(96, 0, 32, 32)), tilemap.tiles, tilemap.images, tilemap.imagerects)
+
+font = pygame.font.SysFont(None, 70)
+gameover = font.render("Game Over!", True, (255, 0, 0))
+over = False
 
 run = True
 dt = pygame.time.get_ticks()
@@ -48,25 +50,34 @@ while run:
     entity.move([sword1, sword2, sword3, box1, box2, enemy1], reciever)
     if entity.health > 0:
         entity.draw()
-    sword1.draw()
-    sword2.draw()
-    sword3.draw()
-    box1.draw()
-    box2.draw()
-    if pygame.time.get_ticks() - dt > 10:
-        enemy1.move_enemy(entity)
-        #enemy2.move_enemy(entity)
-        #enemy3.move_enemy(entity)
-        #enemy4.move_enemy(entity)
-        #enemy5.move_enemy(entity)
-        dt = pygame.time.get_ticks()
-    if enemy1.health > 0:
-        enemy1.draw()
-    #enemy2.draw()
-    #enemy3.draw()
-    #enemy4.draw()
-    #enemy5.draw()
-    reciever.draw(healthbar=False)
+    else:
+        display.blit(gameover, (100, 100))
+        over = True
+    
+    if not over:
+        sword1.draw()
+        sword2.draw()
+        sword3.draw()
+        box1.draw()
+        box2.draw()
+        if pygame.time.get_ticks() - dt > 10:
+            enemy1.move_enemy(entity)
+            enemy2.move_enemy(entity)
+            enemy3.move_enemy(entity)
+            enemy4.move_enemy(entity)
+            enemy5.move_enemy(entity)
+            dt = pygame.time.get_ticks()
+        if enemy1.health > 0:
+            enemy1.draw()
+        if enemy2.health > 0:
+            enemy2.draw()
+        if enemy3.health > 0:
+            enemy3.draw()
+        if enemy4.health > 0:
+            enemy4.draw()
+        if enemy5.health > 0:
+            enemy5.draw()
+        reciever.draw(healthbar=False)
     pygame.display.update()
 
 pygame.quit()
